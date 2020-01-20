f5d39b57cc947786100497e3f2932e3285656dd7
Meka <nmeka@deloitte.com>
2020-01-20 16:03:14 +0530

    book app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// function Greeting() {
+//   return (
+//     //     <section>
+//     //       <div>
+//     //         <h1>this is john and this my first react component</h1>
+//     //       </div>
+//     //       <div>
+//     //         <ul>
+//     //           <li>
+//     //             <a href="#">Home</a>
+//     //           </li>
+//     //         </ul>
+//     //       </div>
+//     //     </section>
+//     //   );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//     <section>
+//       <Person></Person>
+//       <Message></Message>
+//     </section>
+//   );
+// }
+function Books() {
+  return (
+    <section>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+    </section>
+  );
+}
+const Book = () => {
+  return (
+    <article>
+      <CoverImage />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const CoverImage = () => (
+  <img
+    width="200"
+    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
+  />
+);
+const Title = () => <h2>The wonkey donkey</h2>;
+const Author = () => <p>The wonkey Donkey</p>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Books />, document.getElementById("root"));
