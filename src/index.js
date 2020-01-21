56f12d029364009576d75a5e58c62232e99504db
Meka <nmeka@deloitte.com>
2020-01-21 15:09:42 +0530

    props changes

--- src/index.js
+++ src/index.js
@@ -1,60 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// function Greeting() {
-//   return (
-//     //     <section>
-//     //       <div>
-//     //         <h1>this is john and this my first react component</h1>
-//     //       </div>
-//     //       <div>
-//     //         <ul>
-//     //           <li>
-//     //             <a href="#">Home</a>
-//     //           </li>
-//     //         </ul>
-//     //       </div>
-//     //     </section>
-//     //   );
-
-//     <section>
-//       <Person></Person>
-//       <Message></Message>
-//     </section>
-//   );
-// }
-function Books() {
+const btnText = "big button";
+function People() {
   return (
-    <section className="books">
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
+    <section>
+      <Person name="naveen meka" job="developer"></Person>
+      <Person name="Raghu meka" job="engineer"></Person>
+      <Person name="sudheer meka" job="finace" age="23"></Person>
     </section>
   );
 }
-const Book = () => {
+const Person = props => {
+  console.log(props);
   return (
-    <article className="book">
-      <CoverImage />
-      <Title />
-      <Author />
+    <article>
+      <h1>{props.name}</h1>
+      <p>{props.job}</p>
+      <p>
+        <p>{props.age}</p>
+      </p>
+      <hr />
     </article>
   );
 };
-const CoverImage = () => (
-  <img
-    width="200"
-    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
-  />
-);
-const Title = () => (
-  <h2 style={{ fontSize: "16px", color: "yellow" }}>The wonkey donkey</h2>
-);
-const authorStyle = {
-  letterspacing: "10px",
-  color: "green"
-};
-const Author = () => <p style={authorStyle}>The wonkey Donkey</p>;
 
-ReactDOM.render(<Books />, document.getElementById("root"));
+ReactDOM.render(<People />, document.getElementById("root"));
