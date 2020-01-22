5b395fe986445240dd8fb29b0d732c673c084334
Meka <nmeka@deloitte.com>
2020-01-22 11:29:18 +0530

    props advancement

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-const btnText = "big button";
+import ReactDom from "react-dom";
+// import "./index.css";
+
+//DESTRUCTURING
+
 function People() {
+  const friends = [
+    { name: "Naveen", job: "developer", age: 27, company: "deloitte" },
+    { name: "sudhi", job: "finace", age: 23, company: "genpact" },
+    { name: "chandu", job: "designer", age: 27, company: "google" }
+  ];
   return (
     <section>
-      <Person name="naveen meka" job="developer"></Person>
-      <Person name="Raghu meka" job="engineer"></Person>
-      <Person name="sudheer meka" job="finace" age="23"></Person>
+      <Person person={friends[0]} />
+      <Person person={friends[1]} />
+      <Person person={friends[2]} />
     </section>
   );
 }
+
+const person = {
+  name: "Navi",
+  age: 26
+};
+const { name } = person;
+// console.log(name);
+
+const showPerson = ({ name, age }) => console.log(name, age);
+
+showPerson(person);
+
 const Person = props => {
   console.log(props);
+  const { name, job, age, company } = props.person;
+  //({ person: { name, job, age, company } }) => {
+  //const { name, job, age, company } = props.person;
+
   return (
     <article>
-      <h1>{props.name}</h1>
-      <p>{props.job}</p>
-      <p>
-        <p>{props.age}</p>
-      </p>
+      <h1>{name}</h1>
+      <p>{job}</p>
+      <p>{age}</p>
+      <p>{props.person.company}</p>
       <hr />
     </article>
   );
 };
 
-ReactDOM.render(<People />, document.getElementById("root"));
+ReactDom.render(<People />, document.getElementById("root"));
